5358a36b3e2995c0079d45197132d88c9622b86f
shafayetejaman <shafaitzaman.com>
2023-06-14 14:36:34 +0600

    new

--- DSA/linklist.cpp
+++ DSA/linklist.cpp
@@ -17,16 +17,22 @@ public:
 class List
 {
 public:
-    node *head;
+    node *head = nullptr;
     List(int val)
     {
         node *newNode = new node(val);
         head = newNode;
     }
+    List() {}
 
     void append(int val)
     {
         node *newNode = new node(val);
+        if (this->size()== 0)
+        {
+            this->head = newNode;
+            return;
+        }
         node *ptr = this->head;
         while (ptr->next != nullptr)
         {
@@ -44,16 +50,138 @@ public:
         }
         cout << endl;
     }
+    void insert(int index, int val)
+    {
+        node *newNode = new node(val);
+        if (index == 0)
+        {
+            newNode->next = this->head;
+            this->head = newNode;
+            return;
+        }
+        else if (index >= this->size())
+        {
+            // return this->append(val);
+            cout << "Invalid Index!\n";
+            return;
+        }
+        node *ptr = this->head;
+        node *prev = nullptr;
+
+        while (ptr->next != nullptr && index > 0)
+        {
+            prev = ptr;
+            ptr = ptr->next;
+            index--;
+        }
+        prev->next = newNode;
+        newNode->next = ptr;
+    }
+    int size()
+    {
+        int count = 0;
+        node *ptr = this->head;
+        while (ptr != nullptr)
+        {
+            count++;
+            ptr = ptr->next;
+        }
+        return count;
+    }
+    void delete_node(int index)
+    {
+        node *ptr = this->head;
+        node *prev = nullptr;
+        if (index == 0)
+        {
+            this->head = ptr->next;
+            delete ptr;
+            return;
+        }
+        else if (index >= this->size() || index < 0)
+        {
+            // deleting the last node
+            // while (ptr->next != nullptr)
+            // {
+            //     prev = ptr;
+            //     ptr = ptr->next;
+            // }
+            // prev->next = nullptr;
+            // delete ptr;
+
+            cout << "Invalid Index!\n";
+            return;
+        }
+
+        while (ptr->next != nullptr && index > 0)
+        {
+            prev = ptr;
+            ptr = ptr->next;
+            index--;
+        }
+        prev->next = ptr->next;
+        delete ptr;
+    }
+    ~List()
+    {
+        node *ptr = this->head;
+        node *prev = nullptr;
+        while (ptr != nullptr)
+        {
+            prev = ptr;
+            ptr = ptr->next;
+            delete prev;
+        }
+        cout << "All the nodes have been freed\n";
+    }
+    void get_node(int num)
+    {
+        int input;
+        if (this->size() == 0)
+        {
+            this->head = new node(input);
+            node *ptr = this->head;
+
+            while (num--)
+            {
+                cin >> input;
+                node *newNode = new node(input);
+                ptr->next = newNode;
+                ptr = newNode;
+            }
+        }
+        else
+        {
+            while (num--)
+            {
+                cin >> input;
+                this->append(input);
+            }
+        }
+    }
 };
 
 int main()
 {
-    List l(1);
-    l.append(2);
-    l.append(3);
-    l.append(4);
-    l.append(5);
-    l.print();
+    List list(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+    int num;
+    cout << "node length : ";
+    cin >> num;
+
+    list.get_node(num);
+
+    list.print();
+    list.insert(9, 30);
+    list.print();
+    cout << list.size() << endl;
+
+    list.delete_node(6);
+    cout << list.size() << endl;
+    list.print();
 
     return 0;
 }
