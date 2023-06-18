8df55c5674dc173d34abd50f6911efb09deb9549
shafayetejaman <shafaitzaman@gmail.com>
2023-06-18 02:24:36 +0000

    Jun 18, 2023, 8:24 AM

--- DSA/doubly_link_list.cpp
+++ DSA/doubly_link_list.cpp
@@ -1,10 +1,23 @@
 #include "my_hederfile.h"
 
 using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
 
 int main()
 {
-    
+
 
     return 0;
 }
