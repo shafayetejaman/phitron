291b4a1641222eb283d72c1c3c77949cd79cb7d2
shafayetejaman <shafaitzaman@gmail.com>
2023-06-18 17:14:26 +0000

    Jun 18, 2023, 11:14 PM

---
+++ practice problem/list.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class List
+{
+public:
+    node *head;
+    node *tail;
+
+    List(node *head)
+    {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+    void append(int val)
+    {
+        
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
