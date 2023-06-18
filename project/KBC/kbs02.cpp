a20b76d5eb85733c641072b8b4e62fdba922749a
shafayetejaman <shafaitzaman@gmail.com>
2023-06-18 13:22:10 +0000

    Jun 18, 2023, 7:22 PM

--- project/KBC/kbs02.cpp
+++ project/KBC/kbs02.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-#define Q_C 10
 #define QUESTION 0
 #define OPTION 1
 #define ANSWER 2
@@ -10,12 +9,27 @@ using namespace std;
 
 vector<string> get_question()
 {
-    
+    string question, option, answer;
+    cout << "question :\n";
+    cin.ignore();
+    getline(cin, question);
+    int opt = 1;
+    while (opt != 5)
+    {
+        cout << "option" << opt <<":\n ";
+        string s;
+        getline(cin, s);
+        option += (s + '\n');
+        opt++;
+    }
+    cout << "answer :\n ";
+    getline(cin, answer);
+    return {question, option, answer};
 }
 
 int main(void)
 {
-    vector<vector<string>> question = {
+    vector<vector<string>> questions = {
         {{"What is the full form of CPU?\n"},
          {"A : Central Processing Unit\nB : Central Processing System\nC : Central Processor System\nD : Central Processor Unit\n"},
          {"Answer = A : Central Processing Unit\n"}},
@@ -56,39 +70,62 @@ int main(void)
          {"A : Sony\nB : Nintendo\nC : Microsoft\nD : Apple\n"},
          {"Answer = B : Nintendo\n"}}};
 
-    int score = 0;
 
-    for (int i = 0; i < Q_C; i++)
+    while (true)
     {
-        cout << question[i][QUESTION] << endl;
-        cout << question[i][OPTION] << endl;
-
-        string answer;
-        cout << "Type your answer : ";
-        cin >> answer;
-        if (question[i][ANSWER][ANS_C] == toupper(answer[0]))
-            score++;
-        else
+        int quarry;
+        cout << "Enter:\n1 for adding questions\n2 for playing the game\n0 to quit\n$> ";
+        cin >> quarry;
+        if (quarry == 0)
         {
-            cout << "Wrong answer!" << endl;
-            cout << question[i][ANSWER] << endl;
             break;
         }
-        cout << question[i][ANSWER] << endl;
-        cin.clear();
+        else if (quarry == 1)
+        {
+            cout << "Example : ";
+            cout << questions[0][QUESTION] << endl;
+            cout << questions[0][OPTION] << endl;
+            cout <<questions[0][ANSWER]<<endl;
+            questions.push_back(get_question());
+        }
+        else
+        {
+            int score = 0;
+            for (int i = 0; i < questions.size(); i++)
+            {
+                cout << questions[i][QUESTION] << endl;
+                cout << questions[i][OPTION] << endl;
+
+                string answer;
+                cout << "Type your answer : ";
+                cin >> answer;
+                if (questions[i][ANSWER][ANS_C] == toupper(answer[0]))
+                    score++;
+                else
+                {
+                    cout << "Wrong answer!" << endl;
+                    cout << questions[i][ANSWER] << endl;
+                    break;
+                }
+                cout << questions[i][ANSWER] << endl;
+                cin.clear();
+            }
+
+            int money = 0;
+            if (score <= 2 && score > 0)
+                money = 1000;
+            else if (score <= 5 && score > 2)
+                money = 10000;
+            else if (score <= 9 && score > 5)
+                money = 100000;
+            else if (score == 10)
+                money = 1000000;
+
+            cout << "You have earned " << money << '$' << endl;
+        }
     }
 
-    int money = 0;
-    if (score <= 2 && score > 0)
-        money = 1000;
-    else if (score <= 5 && score > 2)
-        money = 10000;
-    else if (score <= 9 && score > 5)
-        money = 100000;
-    else if (score == 10)
-        money = 1000000;
 
-    cout << "You have earned " << money << '$' << endl;
 
     return 0;
 }
