52a13764659989871e3fbb3d3d5e9a6af668ea1a
shafayetejaman <shafaitzaman@gmail.com>
2023-06-18 15:17:47 +0000

    Jun 18, 2023, 9:17 PM

---
+++ practice problem/sting.cpp
@@ -0,0 +1,18 @@
+#include "my_hederfile.h"
+
+using namespace std;
+
+int main()
+{
+    string s = "my name is cat islam , cat goes to cat";
+    string x = "cat";
+
+    
+
+
+
+
+
+
+    return 0;
+}
