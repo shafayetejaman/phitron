806cdcda4ff6371a03b4c2475feadc3a28862fd3
shafayetejaman <shafaitzaman@gmail.com>
2023-06-19 15:54:38 +0000

    Jun 19, 2023, 9:54 PM

---
+++ practice problem/raf/dublelist.cpp
@@ -0,0 +1,28 @@
+#include "my_hederfile.h"
+
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class List
+{
+public:
+
+};
+
+int main()
+{
+
+
+    return 0;
+}
