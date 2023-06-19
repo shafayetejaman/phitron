3937a5ad07dcd67578beb940c9daa076d44bc02d
shafayetejaman <shafaitzaman@gmail.com>
2023-06-19 04:14:15 +0000

    Jun 19, 2023, 10:14 AM

--- DSA/STL_linlkList.cpp
+++ DSA/STL_linlkList.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    
+    list<int> l;
+
+    l.push_back(1);
+    l.push_front(0);
+    l.push_back(2);
+
+    cout << distance(l.begin(), next(l.begin(), 0)) << endl;
 
     return 0;
 }
