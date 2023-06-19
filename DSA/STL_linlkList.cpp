60a543c7fb41cfba7c9b9b9fafbea99de6ea2777
shafayetejaman <shafaitzaman@gmail.com>
2023-06-19 07:48:13 +0000

    Jun 19, 2023, 1:48 PM

--- DSA/STL_linlkList.cpp
+++ DSA/STL_linlkList.cpp
@@ -110,34 +110,36 @@ public:
 
 int main()
 {
-    // list<int> l;
+    list<int> l;
 
-    // l.push_back(6);
-    // l.push_front(9);
-    // l.push_back(2);
+    l.push_back(6);
+    l.push_front(9);
+    l.push_back(2);
 
-    // // cout << *next(l.begin(), 0) << endl;
-    // for (const auto &i : l)
-    // {
-    //     cout << i << " ";
-    // }
-    // cout << endl;
-    // // l.erase(next(l.begin(), 0));
-    // l.sort(greater<int>());
+    // cout << *next(l.begin(), 0) << endl;
+    for (const auto &i : l)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    // l.erase(next(l.begin(), 0));
+    l.sort(greater<int>());
+    
 
-    // for (const auto &i : l)
-    // {
-    //     cout << i << " ";
-    // }
+    for (const auto &i : l)
+    {
+        cout << i << " ";
+    }
 
-    List l;
-    l.append(1);
-    l.append(2);
-    l.append(3);
-    l.append(4);
-    l.append(5);
-    l.reverse(l.head, l.head->next);
-    l.print();
+    // List l;
+    // l.append(1);
+    // l.append(2);
+    // l.append(3);
+    // l.append(4);
+    // l.append(5);
+    // l.reverse(l.head, l.head->next);
+    // l.print();
+    // l.
 
     return 0;
 }
