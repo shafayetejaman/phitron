755f80fed97794edb8a7a053fffef0ac088a12c1
shafayetejaman <shafaitzaman@gmail.com>
2023-06-19 12:07:59 +0000

    Jun 19, 2023, 6:07 PM

--- practice problem/LeetCode/linkListEasy.cpp
+++ practice problem/LeetCode/linkListEasy.cpp
@@ -9,3 +9,28 @@ int main()
     return 0;
 }
 
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *ptr = head;
+        while (ptr->next != nullptr)
+        {
+            if (ptr->val == ptr->next->val)
+            {
+                ptr->next = ptr->next->next;
+            }
+        }
+    }
+};
