ec677429fbe20d61bfb246225d0612eef9a67275
shafayetejaman <shafaitzaman@gmail.com>
2023-06-24 10:12:45 +0000

    Jun 24, 2023, 4:12 PM

--- practice problem/LeetCode/link_list_midiam.cpp
+++ practice problem/LeetCode/link_list_midiam.cpp
@@ -17,6 +17,108 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+            {
+                ListNode *h = head;
+                while (h != slow)
+                {
+                    h = h->next;
+                    slow = slow->next;
+                }
+                return h;
+            }
+        }
+        return NULL;
+    }
+};
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *newList = nullptr;
+        ListNode *curr = head;
+        ListNode *tail = nullptr;
+
+        while (curr != nullptr)
+        {
+            append(newList, curr->val, tail);
+            curr = curr->next;
+        }
+        if (head != nullptr)
+            reverse(head, nullptr, head);
+        curr = head;
+        while (curr != nullptr)
+        {
+            if (curr->val != newList->val)
+            {
+                return false;
+            }
+            newList = newList->next;
+            curr = curr->next;
+        }
+        return true;
+    }
+    void append(ListNode *&head, int val, ListNode *&tail)
+    {
+        ListNode *newNode = new ListNode(val);
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+    }
+    void reverse(ListNode *&head, ListNode *prev, ListNode *curr)
+    {
+        if (curr->next == nullptr)
+        {
+            head = curr;
+            head->next = prev;
+            return;
+        }
+        reverse(head, curr, curr->next);
+        curr->next = prev;
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head != nullptr)
+            reverse(head, nullptr, head);
+        return head;
+    }
+    void reverse(ListNode *&head, ListNode *prev, ListNode *curr)
+    {
+        if (curr->next == nullptr)
+        {
+            head = curr;
+            head->next = prev;
+            return;
+        }
+        reverse(head, curr, curr->next);
+        curr->next = prev;
+    }
+};
 
 class Solution
 {
