9531e0359fe4e2c26f55cc02e80e9b3f27471985
shafayetejaman <shafaitzaman@gmail.com>
2023-06-24 10:39:07 +0000

    Jun 24, 2023, 4:39 PM

--- practice problem/LeetCode/link_list_midiam.cpp
+++ practice problem/LeetCode/link_list_midiam.cpp
@@ -152,3 +152,21 @@ public:
         return head;
     }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution
+ {
+public:
+    ListNode *mergeNodes(ListNode *head)
+    {
+    }
+ };
