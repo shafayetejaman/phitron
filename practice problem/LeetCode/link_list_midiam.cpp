5c820670dd1755056a8d1f259fb4b34a46ff3b45
shafayet <shafaitzaman@gmail.com>
2023-06-26 15:58:02 +0000

    Jun 26, 2023, 9:58 PM

--- practice problem/LeetCode/link_list_midiam.cpp
+++ practice problem/LeetCode/link_list_midiam.cpp
@@ -151,68 +151,7 @@ public:
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
-public:
-    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
-    {
-        if (list1 == nullptr)
-        {
-            return list2;
-        }
-        else if (list2 == nullptr)
-        {
-            return list1;
-        }
-        ListNode *ans;
-        if (list1->val < list2->val)
-        {
-            ListNode *newNode = new ListNode(list1->val);
-            ans = newNode;
-            list1 = list1->next;
-        }
-        else
-        {
-            ListNode *newNode = new ListNode(list2->val);
-            ans = newNode;
-            list2 = list2->next;
-        }
-        ListNode *curr = ans;
-        while (list1 != nullptr && list2 != nullptr)
-        {
-            if (list1->val < list2->val)
-            {
-                ListNode *newNode = new ListNode(list1->val);
-                curr->next = newNode;
-                curr = curr->next;
-                list1 = list1->next;
-            }
-            else
-            {
-                ListNode *newNode = new ListNode(list2->val);
-                curr->next = newNode;
-                curr = curr->next;
-                list2 = list2->next;
-            }
-        }
-        while (list1 != nullptr)
-        {
 
-            ListNode *newNode = new ListNode(list1->val);
-            curr->next = newNode;
-            curr = curr->next;
-            list1 = list1->next;
-        }
-        while (list2 != nullptr)
-        {
-            ListNode *newNode = new ListNode(list2->val);
-            curr->next = newNode;
-            curr = curr->next;
-            list2 = list2->next;
-        }
-        return ans;
-    }
-};
 class Solution
 {
 public:
