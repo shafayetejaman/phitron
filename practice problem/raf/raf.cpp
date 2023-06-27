a7a4019294ef7a4960ec14fcba5abb8af365ac13
shafayet <shafaitzaman@gmail.com>
2023-06-27 12:13:35 +0000

    Jun 27, 2023, 6:13 PM

--- practice problem/raf/raf.cpp
+++ practice problem/raf/raf.cpp
@@ -2,6 +2,68 @@
 
 using namespace std;
 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        if (left == right)
+        {
+            return head;
+        }
+        ListNode *prev = head;
+        ListNode *prevPrev = nullptr; // 1 2 3 3 4   left 2 right 4
+        ListNode *last = head;
+        while (--left)
+        {
+            prevPrev = prev;
+            prev = prev->next;
+        }
+        while (--right)
+        {
+            last = last->next;
+        }
+        if (prevPrev != nullptr)
+            prevPrev->next = nullptr;
+        ListNode *r_head = prev;
+        ListNode *r_tail = last;
+        r_tail->next = nullptr;
+        reverse(r_head, nullptr, r_head, r_tail);
+
+        r_tail->next = last->next;
+
+        if (prevPrev == nullptr)
+        {
+            return head;
+        }
+        else
+        {
+            prevPrev->next = r_head;
+            return head;
+        }
+        return head;
+    }
+    void reverse(ListNode *&head, ListNode *prev, ListNode *curr, ListNode *&tail)
+    {
+        if (curr == nullptr)
+        {
+            tail = head;
+            head = prev;
+            return;
+        }
+        reverse(head, curr, curr->next, tail);
+        curr->next = prev;
+    }
+};
 int main()
 {
     int n = 10;
