9cd205b61a90275d2733d62ee521a226554da8c6
shafayet <shafaitzaman@gmail.com>
2023-06-27 04:40:03 +0000

    Jun 27, 2023, 10:40 AM

---
+++ practice problem/raf/list.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+int size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+void print_linekd_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_tail(head, tail, val);
+    }
+
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+    int val2;
+    while (true)
+    {
+        cin >> val2;
+        if (val2 == -1)
+            break;
+        insert_tail(head2, tail2, val2);
+    }
+    int size1 = size(head);
+    int size2 = size(head2);
+    int flag = 0;
+    if (size1 == size2)
+    {
+        for (Node *i = head; i != NULL; i = i->next)
+        {
+            for (Node *j = head2; j >= i->next; j = j->next)
+            {
+
+                if (j == i)
+                {
+
+                    if (i->val != j->val)
+                    {
+                        flag++;
+                        break;
+                    }
+                    else
+                    {
+                        flag++;
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    else
+    {
+        flag++;
+    }
+
+    if (flag == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
