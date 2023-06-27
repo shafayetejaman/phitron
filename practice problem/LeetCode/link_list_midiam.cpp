364385a5cfee67e77bdf981098e8257529c4fa6b
shafayet <shafaitzaman@gmail.com>
2023-06-27 10:15:38 +0000

    Jun 27, 2023, 4:15 PM

--- practice problem/LeetCode/link_list_midiam.cpp
+++ practice problem/LeetCode/link_list_midiam.cpp
@@ -314,3 +314,36 @@ public:
         return mid;
     }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        ListNode *prev = head;
+        while (--left)
+        {
+
+        }
+    }
+    void reverse(ListNode *&head, ListNode *prev, ListNode *curr, ListNode *&tail)
+    {
+        if (curr == nullptr)
+        {
+            tail = head;
+            head = prev;
+            return;
+        }
+        reverse(head, curr, curr->next, tail);
+        curr->next = prev;
+    }
+};
