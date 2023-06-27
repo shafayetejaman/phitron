59903a07d02bbcde1ff3bf2830181bf38f88219c
shafayet <shafaitzaman@gmail.com>
2023-06-27 05:01:09 +0000

    Jun 27, 2023, 11:01 AM

---
+++ practice problem/raf/raf.cpp
@@ -0,0 +1,16 @@
+#include "my_hederfile.h"
+
+using namespace std;
+
+int main()
+{
+    int n = 10;
+        //             9 == 0
+    for (auto i = n-1; i > -1; i--)
+    {
+        cout << i << " ";
+    }
+    print();
+
+    return 0;
+}
