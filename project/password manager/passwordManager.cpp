94df38df1aa71dbe406ea49566fd73a251892c5c
shafayet <shafaitzaman@gmail.com>
2023-06-28 17:14:22 +0000

    Jun 28, 2023, 11:14 PM

--- project/password manager/passwordManager.cpp
+++ project/password manager/passwordManager.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+class Array
+{
+public:
+    string first;
+    string second;
+    Array(string first, string second)
+    {
+        this->first = first;
+        this->second = second;
+    }
+};
+
+int MAX;
+
+class PassWord
+{
+private:
+    vector<Array> Map;
+
+public:
+    
+};
+
 int main()
 {
-    
+
 
     return 0;
 }
