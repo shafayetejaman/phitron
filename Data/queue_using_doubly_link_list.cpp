80f8df3d087b75b148fdd5cbea56ec102ae53a7a
shafayet <shafaitzaman@gmail.com>
2023-07-02 16:54:34 +0000

    Jul 2, 2023, 10:54 PM

--- Data/queue_using_doubly_link_list.cpp
+++ Data/queue_using_doubly_link_list.cpp
@@ -2,9 +2,93 @@
 
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
+class Queue
+{
+private:
+    Node *frontNode = nullptr;
+    Node *backNode = nullptr;
+    int elementSize = 0;
+
+public:
+    Queue(int data)
+    {
+        Node *newNode = new Node(data);
+        this->frontNode = newNode;
+        this->backNode = newNode;
+        this->elementSize++;
+    }
+
+    void push(int data)
+    {
+        Node *newNode = new Node(data);
+        this->backNode->next = newNode;
+        newNode->prev = this->backNode;
+        this->backNode = newNode;
+        this->elementSize++;
+    }
+
+    int pop()
+    {
+        if (this->frontNode == nullptr)
+        {
+            cout << "the queue is empty";
+            return -1;
+        }
+        Node *temp = this->frontNode;
+        this->frontNode = this->frontNode->next;
+        int data = temp->data;
+        delete temp;
+        this->elementSize--;
+        return data;
+    }
+    int front()
+    {
+        return this->frontNode->data;
+    }
+    int back()
+    {
+        return this->backNode->data;
+    }
+    bool empty()
+    {
+        if (this->frontNode == nullptr)
+        {
+            return true;
+        }
+        return false;
+    }
+    int size()
+    {
+        return elementSize;
+    }
+};
+
 int main()
 {
-    
+    Queue queue(1);
+    for (auto i = 2; i <= 5; i++)
+    {
+        queue.push(i); // 1 2 3  4 5
+    }
+    while (!queue.empty())
+    {
+        cout << queue.pop() << " " << queue.size() << endl;
+    }
 
     return 0;
 }
