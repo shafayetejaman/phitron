9d7cf10bcc6d672ecd2582082ac12bbb2904d45a
shafayet <shafaitzaman@gmail.com>
2023-07-02 15:40:18 +0000

    Jul 2, 2023, 9:40 PM

--- Data/queue_using_singly_link_list.cpp
+++ Data/queue_using_singly_link_list.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class queue
+{
+public:
+    node *front = nullptr;
+    node *back = nullptr;
+    
+};
+
 int main()
 {
-    
+
 
     return 0;
 }
