bacacd8a139cdeed2322c0484e6201c08900eef6
shafayet <shafaitzaman@gmail.com>
2023-07-02 15:45:00 +0000

    Jul 2, 2023, 9:45 PM

--- Data/queue_using_singly_link_list.cpp
+++ Data/queue_using_singly_link_list.cpp
@@ -19,13 +19,24 @@ class queue
 public:
     node *front = nullptr;
     node *back = nullptr;
+
     queue(int data)
     {
         node *newNode = new node(data);
         this->front = newNode;
         this->back = newNode;
     }
-    
+
+    void push(int data)
+    {
+        node *newNode = new node(data);
+        this->back->next = newNode;
+        this->back = newNode;
+    }
+    int pop()
+    {
+        
+    }
 };
 
 int main()
