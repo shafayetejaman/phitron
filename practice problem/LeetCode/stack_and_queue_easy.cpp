7850e25237468aa42b93b79ecee4ff62f2221aa8
shafayet <shafaitzaman@gmail.com>
2023-07-03 13:24:36 +0000

    Jul 3, 2023, 7:24 PM

--- practice problem/LeetCode/stack_and_queue_easy.cpp
+++ practice problem/LeetCode/stack_and_queue_easy.cpp
@@ -44,7 +44,23 @@ public:
 
     int top()
     {
-        return q1.back();
+        queue<int> q2;
+        int topVal;
+        while (!q1.empty())
+        {
+            if (q1.size() != 1)
+            {
+                q2.push(q1.front());
+            }
+            else
+            {
+                topVal = q1.front();
+            }
+
+            q1.pop();
+        }
+        q1 = q2;
+        return topVal;
     }
 
     bool empty()
@@ -55,23 +71,85 @@ public:
 class MyQueue
 {
 public:
+    stack<int> q1;
     MyQueue()
     {
     }
 
     void push(int x)
     {
+        q1.push(x);
     }
 
     int pop()
     {
-    }
+        stack<int> q2;
+        int topVal;
+        while (!q1.empty())
+        {
+            if (q1.size() != 1)
+            {
+                q2.push(q1.top());
+            }
+            else
+            {
+                topVal = q1.top();
+            }
 
+            q1.pop();
+        }
+
+        while (!q2.empty())
+        {
+
+            q1.push(q2.top());
+
+            q2.pop();
+        }
+
+        return topVal;
+    }
     int peek()
     {
+        stack<int> q2;
+        int topVal;
+        while (!q1.empty())
+        {
+
+            q2.push(q1.top());
+            if (q1.size() == 1)
+            {
+                topVal = q1.top();
+            }
+
+            q1.pop();
+        }
+
+        while (!q2.empty())
+        {
+
+            q1.push(q2.top());
+
+            q2.pop();
+        }
+        return topVal;
+    }
+
+    int top()
+    {
+        stack<int> q2;
+
+        while (!q1.empty())
+        {
+            q2.push(q1.top());
+            q1.pop();
+        }
+        q1 = q2;
+        return q2.top();
     }
 
     bool empty()
     {
+        return q1.empty();
     }
 };
