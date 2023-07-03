58bc9326768094d7e7774dd2ac52463d38602b5c
shafayet <shafaitzaman@gmail.com>
2023-07-03 10:56:34 +0000

    Jul 3, 2023, 4:56 PM

--- practice problem/LeetCode/stack_and_queue_easy.cpp
+++ practice problem/LeetCode/stack_and_queue_easy.cpp
@@ -12,24 +12,36 @@ int main()
 class MyStack
 {
 public:
+    queue<int> q1, q2;
     MyStack()
     {
-        queue<int> q1, q2;
     }
 
     void push(int x)
     {
+        q1.push(x);
     }
 
     int pop()
     {
+        int topVal;
+        while (!q1.empty())
+        {
+            q2.push(q1.front());
+            topVal = q1.front();
+            q1.pop();
+        }
+        q1 = q2;
+        return topVal;
     }
 
     int top()
     {
+       return q1.back();
     }
 
     bool empty()
     {
+       return q1.empty();
     }
 };
