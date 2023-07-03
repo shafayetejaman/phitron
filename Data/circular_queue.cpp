672d1b200e28abae9ff9aa1a44ef226f8995b598
shafayet <shafaitzaman@gmail.com>
2023-07-03 05:36:30 +0000

    Jul 3, 2023, 11:36 AM

--- Data/circular_queue.cpp
+++ Data/circular_queue.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+class Queue
+{
+private:
+    int size;
+    int frontIndex;
+    int backIndex;
+    vector<int> arr;
+
+public:
+    Queue(int size)
+    {
+        this->size = size;
+        this->frontIndex = 0;
+        this->backIndex = 0;
+        vector<int> arr(size);
+        this->arr = arr;
+    }
+
+    void push(int data)
+    {
+        
+    }
+};
+
 int main()
 {
-    
 
     return 0;
 }
