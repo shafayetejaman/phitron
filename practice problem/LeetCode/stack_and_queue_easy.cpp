8aa431db3ec94f11d6d86ab4ea39ccedb809f655
shafayet <shafaitzaman@gmail.com>
2023-07-03 10:50:00 +0000

    Jul 3, 2023, 4:50 PM

--- practice problem/LeetCode/stack_and_queue_easy.cpp
+++ practice problem/LeetCode/stack_and_queue_easy.cpp
@@ -4,7 +4,32 @@ using namespace std;
 
 int main()
 {
-    
+
 
     return 0;
 }
+
+class MyStack
+{
+public:
+    MyStack()
+    {
+        queue<int> q1, q2;
+    }
+
+    void push(int x)
+    {
+    }
+
+    int pop()
+    {
+    }
+
+    int top()
+    {
+    }
+
+    bool empty()
+    {
+    }
+};
