7dd656a4df6e788e45ffe322e7930f5f9b220927
shafayet <shafaitzaman@gmail.com>
2023-07-09 16:11:20 +0000

    Jul 9, 2023, 10:11 PM

--- container/binary_tree.cpp
+++ container/binary_tree.cpp
@@ -1,10 +1,38 @@
 #include "my_hederfile.h"
 
 using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->right = nullptr;
+        this->left = nullptr;
+    }
+};
+
+class Tree
+{
+public:
+    Node *root;
+    Tree()
+    {
+        this->root = nullptr;
+    }
+    Tree(int data)
+    {
+        this->root = nullptr;
+    }
+};
 
 int main()
 {
-    
+
 
     return 0;
 }
