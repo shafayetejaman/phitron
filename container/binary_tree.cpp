203af2e587201440c0f00db5671dd5e4718bfbbe
shafayet <shafaitzaman@gmail.com>
2023-07-09 16:28:39 +0000

    Jul 9, 2023, 10:28 PM

--- container/binary_tree.cpp
+++ container/binary_tree.cpp
@@ -31,22 +31,52 @@ public:
     }
     void add(int data)
     {
-
+        helper_add(data, this->root);
     }
-    void insert(int data, Node *ptr)
+    void helper_add(int data, Node *ptr)
+    {
+        if (ptr->left == nullptr || ptr->right == nullptr)
+        {
+            Node *newNode = new Node(data);
+
+            if (ptr->left != nullptr)
+            {
+                if (ptr->left->data > data)
+                {
+                    ptr->right = newNode;
+                }
+                else
+                {
+                    ptr->left = newNode;
+                }
+            }
+        }
+        helper_add(data, ptr->left);
+        helper_add(data, ptr->right);
+    }
+    void print()
+    {
+        helper_print(this->root);
+    }
+    void helper_print(Node *ptr)
     {
         if (ptr == nullptr)
         {
-            
+            return;
         }
+        helper_print(ptr->left);
+        helper_print(ptr->right);
     }
-
 };
 
 int main()
 {
-    Tree tree(1);
-
+    Tree tree(10);
+    tree.add(20);
+    tree.add(25);
+    tree.add(30);
+    tree.add(35);
+    tree.print();
 
     return 0;
 }
