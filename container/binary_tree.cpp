7fddf7cc4662ea67340f1e53fa4da618b1cbf68d
shafayet <shafaitzaman@gmail.com>
2023-07-10 05:51:13 +0000

    Jul 10, 2023, 11:51 AM

--- container/binary_tree.cpp
+++ container/binary_tree.cpp
@@ -85,11 +85,11 @@ public:
 
 int main()
 {
-    Tree tree(1);
+    Tree tree(10);
     tree.insert(20);
-    tree.insert(25);
     tree.insert(30);
-    tree.insert(35);
+    tree.insert(30);
+    tree.insert(40);
     tree.print();
 
     return 0;
